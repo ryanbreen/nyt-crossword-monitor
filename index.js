fd9ede07e026876985587c6506c8d43254afc202
Ryan Breen <rbreen@zmags.com>
2022-02-25 16:31:38 -0500

    Switch to tracking the actual streak since date_end wasn't accurate.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import 'dotenv/config';
 
 import { format } from 'date-fns';
@@ -10,20 +9,25 @@ const twilioClient = twilio(process.env.TWILIO_KEY, process.env.TWILIO_SECRET);
 
 const STATS_URL = `https://nyt-games-prd.appspot.com/svc/crosswords/v3/${process.env.SUBSCRIBER_ID}/stats-and-streaks.json?date_start=2014-01-01&start_on_monday=true`
 
+const STREAK_START = process.env.STREAK_START;
+
 const poller = async () => {
 
+    try {
     const response = await fetch(STATS_URL);
     const stats = await response.json();
 
     const today = format(new Date(), 'yyyy-MM-dd');
 
-    const lastDone = stats && stats.results && stats.results.streaks && stats.results.streaks['date_end'];
-
     const now = new Date();
-    console.log(`[${now}] ${now.getHours()} -- ${today === lastDone ? '✅' : '😬' }`);
+    const fullDaysSinceEpoch = Math.floor(now/8.64e7);
+
+    const done = fullDaysSinceEpoch - stats.results.streaks.current_streak >= STREAK_START;
+
+    console.log(`[${now}] Streak is at ${stats.results.streaks.current_streak} ${done ? '✅' : '😬' }`);
 
     // if the puzzle isn't done and it's past noon, text to yell at me
-    if (today !== lastDone && now.getHours() > 12) {
+    if (!done && now.getHours() > 12) {
         console.log('Call me maybe');
 
         try {
@@ -36,6 +40,9 @@ const poller = async () => {
             console.error('Could not notify administrator');
             console.error(e);
         }
+    }
+    } catch(e) {
+        console.log(`Failed to run check due to ${e}`);
     }
 
 }
