4511e96fce6c92f463e95e7b9d83fb06d7e5b479
Ryan Breen <rbreen@zmags.com>
2023-10-09 04:11:48 -0400

    Fix hostname of API

--- index.js
+++ index.js
@@ -1,54 +1,55 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import { format } from 'date-fns';
+import { format } from "date-fns";
 
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-import twilio from 'twilio'
+import twilio from "twilio";
 const twilioClient = twilio(process.env.TWILIO_KEY, process.env.TWILIO_SECRET);
 
-const STATS_URL = `https://nyt-games-prd.appspot.com/svc/crosswords/v3/${process.env.SUBSCRIBER_ID}/stats-and-streaks.json?date_start=2014-01-01&start_on_monday=true`
+const STATS_URL = `https://www.nytimes.com/svc/crosswords/v3/${process.env.SUBSCRIBER_ID}/stats-and-streaks.json?date_start=2014-01-01&start_on_monday=true`;
 
-const STREAK_START = process.env.STREAK_START;
+const STREAK_OFFSET = process.env.STREAK_OFFSET;
 
 const poller = async () => {
-
     try {
-    const response = await fetch(STATS_URL);
-    const stats = await response.json();
+        const response = await fetch(STATS_URL);
 
-    const today = format(new Date(), 'yyyy-MM-dd');
-
-    const now = new Date();
-    const fullDaysSinceEpoch = Math.floor(now/8.64e7);
-
-    const daysSinceStreakStart = fullDaysSinceEpoch - STREAK_START;
-    console.log(`There have been ${daysSinceStreakStart} calendar days since the start of the streak`);
-
-    const done = stats.results.streaks.current_streak >= daysSinceStreakStart;
-
-    console.log(`[${now}] Streak is at ${stats.results.streaks.current_streak} ${done ? '✅' : '😬' }`);
-
-    // if the puzzle isn't done and it's past noon, text to yell at me
-    if (!done && now.getHours() > 12) {
-        console.log('Call me maybe');
-
-        try {
-            await twilioClient.messages.create({
-                body: "Finish the crossword!",
-                to: process.env.TWILIO_TO,
-                from: process.env.TWILIO_FROM
-            });
-        } catch(e) {
-            console.error('Could not notify administrator');
-            console.error(e);
+        // Debugging: Log HTTP Status Code and Response Text
+        if (!response.ok) {
+            console.log(`Received a ${response.status} status`);
+            const text = await response.text();
+            console.log(`Response body: ${text}`);
+            return;
         }
-    }
-    } catch(e) {
+
+        const stats = await response.json();
+
+        const today = format(new Date(), "yyyy-MM-dd");
+
+        const done = stats.results.streaks.dates[STREAK_OFFSET][1] >= today;
+
+        console.log(`[${new Date()}] Streak is at ${stats.results.streaks.current_streak} through ${stats.results.streaks.dates[STREAK_OFFSET][1]} ${done ? "✅" : "😬"}`);
+
+        // if the puzzle isn't done and it's past noon, text to yell at me
+        if (!done && new Date().getHours() > 12) {
+            console.log("Call me maybe");
+
+            try {
+                await twilioClient.messages.create({
+                    body: "Finish the crossword!",
+                    to: process.env.TWILIO_TO,
+                    from: process.env.TWILIO_FROM,
+                });
+            } catch (e) {
+                console.error("Could not notify administrator");
+                console.error(e);
+            }
+        }
+    } catch (e) {
         console.log(`Failed to run check due to ${e}`);
     }
-
-}
+};
 
 poller();
-setInterval(poller, 60*60*1000);
+setInterval(poller, 60 * 60 * 1000);
